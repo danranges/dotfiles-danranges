990cca97d65fe75b7d4c08809f40e939786ea271
Dan Ranges <danranges@gmail.com>
2022-05-19 21:15:02 +0200

    Add colorscheme.lua

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,13 @@
+local colorscheme = "NeoSolarized"
+
+vim.api.nvim_command([[
+    augroup ChangeBackgroudColour
+        autocmd colorscheme * :hi normal guibg=none
+    augroup END
+]])
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
