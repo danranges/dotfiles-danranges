58995458ab1f7eac6df0b1fad4a6b8295d8bb038
Dan Ranges <danranges@gmail.com>
2022-05-24 18:27:55 +0200

    Add autopairs

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -9,7 +9,6 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
@@ -80,13 +79,9 @@ telescope.setup {
     },
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+    find_files = {
+      theme = "dropdown",
+    }
   },
   extensions = {
     media_files = {
