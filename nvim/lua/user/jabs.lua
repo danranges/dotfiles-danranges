28de25a71efcb0722cbebb7f81f18677c2bad540
Dan Ranges <danranges@gmail.com>
2022-05-25 22:48:25 +0200

    Fix FloatNormal guibg

--- nvim/lua/user/jabs.lua
+++ nvim/lua/user/jabs.lua
@@ -3,13 +3,15 @@ require 'jabs'.setup {
     position = 'center', -- center, corner. Default corner
     width = 60, -- default 50
     height = 20, -- default 10
-    border = 'solid', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default shadow
+    border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default shadow
 
     -- Options for preview window
     -- preview_position = 'left', -- top, bottom, left, right. Default top
     preview = {
     --     width = 40, -- default 70
     --     height = 60, -- default 30
-        border = 'solid', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default double
+        border = 'rounded', -- none, single, double, rounded, solid, shadow, (or an array or chars). Default double
     },
 }
+
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = "none" })
