8a02855fbc96173023032a0b9480cbd1eb02f34c
Dan Ranges <danranges@gmail.com>
2022-05-30 23:28:24 +0200

    Add tabline

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -52,6 +52,8 @@ return packer.startup(function(use)
 	use("nvim-lualine/lualine.nvim")
 	use("akinsho/toggleterm.nvim")
 	use("lewis6991/impatient.nvim")
+	use("kdheepak/tabline.nvim")
+	-- use("akinsho/bufferline.nvim")
 
 	-- Colorschemes
 	use("overcache/NeoSolarized")
